4ac05c642bbb1af30722a5d3603b8370e6c7382a
firefield2 <wojtekgomola@gmail.com>
2017-01-08 18:49:26 +0100

    Add some crazy shit.

--- ELEMENTarz/ELEMENTarz/Form1.Designer.cs
+++ ELEMENTarz/ELEMENTarz/Form1.Designer.cs
@@ -28,12 +28,62 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.data_View = new System.Windows.Forms.DataGridView();
+            this.search_btn = new System.Windows.Forms.Button();
+            this.toSearch_tb = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.data_View)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // data_View
+            // 
+            this.data_View.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.data_View.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.data_View.Location = new System.Drawing.Point(12, 12);
+            this.data_View.Name = "data_View";
+            this.data_View.Size = new System.Drawing.Size(260, 199);
+            this.data_View.TabIndex = 0;
+            // 
+            // search_btn
+            // 
+            this.search_btn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.search_btn.Location = new System.Drawing.Point(12, 226);
+            this.search_btn.Name = "search_btn";
+            this.search_btn.Size = new System.Drawing.Size(75, 23);
+            this.search_btn.TabIndex = 1;
+            this.search_btn.Text = "Wyszukaj";
+            this.search_btn.UseVisualStyleBackColor = true;
+            this.search_btn.Click += new System.EventHandler(this.search_btn_Click);
+            // 
+            // toSearch_tb
+            // 
+            this.toSearch_tb.Location = new System.Drawing.Point(118, 228);
+            this.toSearch_tb.Name = "toSearch_tb";
+            this.toSearch_tb.Size = new System.Drawing.Size(154, 20);
+            this.toSearch_tb.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.toSearch_tb);
+            this.Controls.Add(this.search_btn);
+            this.Controls.Add(this.data_View);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.data_View)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView data_View;
+        private System.Windows.Forms.Button search_btn;
+        private System.Windows.Forms.TextBox toSearch_tb;
     }
 }
 
---
+++ ELEMENTarz/ELEMENTarz/Shops.cs
@@ -0,0 +1,62 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ELEMENTarz
+{
+    public class Result
+    {
+        public string Name { get; set; }
+        public string Price { get; set; }
+    }
+    public class ShopList
+    {
+        public List<Shop> ListOfShops;
+        public ShopList()
+        {
+            ListOfShops = new List<Shop>
+            {
+                new Lispol(),
+                new Elbrod()
+            };
+        }
+    }
+    public class Shop
+    {
+        public string NameNode { get; set; }
+        public string PriceNode { get; set; }
+        public string Searcher { get; set; }
+        public string Name { get; set; }
+        public List<Result> resoults;
+
+        public Shop()
+        {
+
+        }
+    }
+    public class Lispol : Shop
+    {
+        public Lispol()
+        {
+            NameNode = "/html[1]/body[1]/div[5]/div[1]/div[1]/div[2]/div[2]/div[1]/table[1]/tr//td//div[1]/div[2]/div[1]/div[2]/div[1]";
+            PriceNode = "/html[1]/body[1]/div[5]/div[1]/div[1]/div[2]/div[2]/div[1]/table[1]/tr//td/div[1]/div[2]/div[2]/div[1]/p[2]";
+            Searcher = "http://lispol.com/oferta-szukaj/";
+            Name = "Lispol";
+        }
+    }
+    public class Elbrod : Shop
+    {
+        public Elbrod()
+        {
+            NameNode = "/html[1]/body[1]/div[1]/div[6]/section[1]/div[1]/div//div[2]/h3[1]/a[1]";
+            PriceNode = "/html[1]/body[1]/div[1]/div[6]/section[1]/div[1]/div//div[2]/span[1]/em[1]/text()";
+            Searcher = "http://elbrod.pl/szukaj.html/szukaj=";
+            Name = "Elbrod";
+        }
+    }
+
+}
